b3d859f6bd53a96139f9f0c2fbb3d6ac7d906df6
Настя Каршакевич <n.karshak@gmail.com>
2022-08-23 18:22:20 +0300

    second task

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from pprint import pprint
-import json
+import os
+
+
 
 class Superhero:
 
@@ -27,3 +29,37 @@ if __name__ == '__main__':
             max_brain = item['intelligence']
             smartest_hero = item['name']
     print(smartest_hero)
+
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, path_to_file):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": path_to_file, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, path_to_file, file_name):
+        href = self._get_upload_link(path_to_file=path_to_file).get("href", "")
+        response = requests.put(href, data=open(file_name, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    file_name = 'Superheroes_api.txt'
+    token = ''
+    uploader = YaUploader(token)
+    result = uploader.upload('folder/heroes','Superheroes_api.txt')
